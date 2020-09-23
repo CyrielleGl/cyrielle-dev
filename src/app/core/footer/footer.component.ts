553146011f054879f04b429417e988c0a7dc9a57
CyrielleGl <cyrielle.gallou@gmail.com>
2020-09-24 00:22:02 +0200

    back to top

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -13,7 +14,28 @@ export class FooterComponent implements OnInit {
   @Input()
   appAuthor: string;
 
-  constructor() { }
+  windowScrolled: boolean;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      }
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+  scrollToTop() {
+      (function smoothscroll() {
+          const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+          }
+      })();
+  }
 
   ngOnInit(): void {
   }
