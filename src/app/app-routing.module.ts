7774c32ac8fffeb0b0b23fbd51d83595dd426185
CyrielleGl <cyrielle.gallou@gmail.com>
2020-09-23 23:53:58 +0200

    creation components & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
 import { MentionsLegalesComponent } from './mentions-legales/mentions-legales.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReferenceComponent } from './reference/reference.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'experience', component: ExperienceComponent },
   { path: 'mentions-legales', component: MentionsLegalesComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'reference', component: ReferenceComponent },
+  { path: 'skills', component: SkillsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,25 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { MentionsLegalesComponent } from './mentions-legales/mentions-legales.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ReferenceComponent } from './reference/reference.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MentionsLegalesComponent
+    MentionsLegalesComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    EducationComponent,
+    ReferenceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
